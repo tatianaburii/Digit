17feb5edbb0cb1dc974ae8972d484bc306753697
tatianaburii <azazazazazaz3333@gmail.com>
2022-02-11 15:56:14 +0200

    Initial commit

---
+++ src/main/java/DigitService.java
@@ -0,0 +1,24 @@
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class DigitService {
+
+    Digit[] digits = Digit.createDigital(
+            "  ###      #     #####   #####  #       #######  #####  #######  #####   ##### ",
+            " #   #    ##    #     # #     # #    #  #       #     # #    #  #     # #     #",
+            "#     #  # #          #       # #    #  #       #           #   #     # #     #",
+            "#     #    #     #####   #####  #    #  ######  ######     #     #####   ######",
+            "#     #    #    #             # #######       # #     #   #     #     #       #",
+            " #   #     #    #       #     #      #  #     # #     #   #     #     # #     #",
+            "  ###    #####  #######  #####       #   #####   #####    #      #####   ##### "
+    );
+    public void print(int[] number){
+        int height = digits[0].lines.length;
+        for (int i = 0; i < height; i++) {
+            for (int digit : number) {
+                System.out.print(digits[digit].lines[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
